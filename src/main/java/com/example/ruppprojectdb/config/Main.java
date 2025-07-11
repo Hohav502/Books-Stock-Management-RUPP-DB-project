8a116de03b9729afdce42a7932c3e105f5d13335
Hohav502 <havho3665@email.com>
2025-07-11 08:34:45 +0700

    First commit

---
+++ src/main/java/com/example/ruppprojectdb/config/Main.java
@@ -0,0 +1,5 @@
+package com.example.ruppprojectdb.config;
+
+public class Main {
+
+}
---
+++ src/test/java/com/example/ruppprojectdb/RuppProjectDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ruppprojectdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuppProjectDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
